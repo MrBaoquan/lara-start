ef18ff33878c6e87e833658df225f8e08958006e
mrbaoquan <mrma617@gmail.com>
2021-03-01 17:25:04 +0800

    add userinfo route.

--- routes.php
+++ routes.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Mrba\LaraStart\Controllers\AuthController;
+use Mrba\LaraStart\Resources\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,11 @@ Route::group(['middleware' => ['wechat.mock']], function () {
 // 使用微信授权登录代理 获取网页授权用户信息
 Route::any('/proxy/auth/wechat', [AuthController::class, 'ProxyAuthWechat'])
     ->middleware('proxy.wechat.oauth');
+
+
+Route::group(['prefix' => 'api', 'middleware' => ['api', 'auth:sanctum']], function () {
+    // example: 当前已认证用户信息
+    Route::get('/userinfo', function () {
+        return new User(Auth::user());
+    });
+});
